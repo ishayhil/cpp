b2acff5725473171f69d81c88798785cb25b0cf3
Ishay Hilzenrat <ishay.hil@gmail.com>
2020-01-04 19:27:26 +0200

    passed presubmittion

--- FractalDrawer/FractalDrawer.cpp
+++ FractalDrawer/FractalDrawer.cpp
@@ -2,23 +2,36 @@
 #include <iostream>
 #include "Fractal.h"
 
+/**
+ * prints the fractals by the definition in the given path
+ * @param path the given csv path
+ */
 void printFractals(std::string &path)
 {
     FractalFactory::parseCsv(path);
     std::vector<Fractal *> &v = FractalFactory::fractals;
     for (int i = (int) v.size() - 1; i > -1; --i)
     {
-        v[i]->populate();
-        v[i]->print();
-        if (i != -1) { // todo change it or now?
+        v.at(i)->populate();
+        v.at(i)->print();
+        if (i != -1)
+        {
             std::cout << std::endl << std::endl;
-        } else {
+        }
+        else
+        {
             std::cout << std::endl;
         }
     }
     FractalFactory::deleteFractals();
 }
 
+/**
+ * the main function. prints the fractals by the definition in the given path
+ * @param argc number of args (min = 1)
+ * @param args the program args
+ * @return EXIT_SUCCESS if success, EXIT_FAILURE otherwise.
+ */
 int main(int argc, char **args)
 {
     if (argc != 2)
@@ -32,4 +45,5 @@ int main(int argc, char **args)
         path += args[1][i];
     }
     printFractals(path);
+    return EXIT_SUCCESS;
 }
