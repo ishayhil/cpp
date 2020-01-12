3174c9e9e13e8068209b0153b49fd7c61ff451c7
Ishay Hilzenrat <ishay.hil@gmail.com>
2020-01-12 14:51:08 +0200

    finished most of hashMap api

--- ex3/HashMap.cpp
+++ ex3/HashMap.cpp
@@ -4,20 +4,52 @@
 
 #include "HashMap.hpp"
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::string;
 using std::endl;
 
 
-int main() {
-    HashMap<string, int> h;
+int main()
+{
+    HashMap<string, double> h;
+    for (int i = 0; i < 16; ++i)
+    {
+        h.insert("ishay_" + std::to_string(i), i);
+    }
+
+    for (int i = 0; i < 16; ++i)
+    {
+        h.erase("ishay_" + std::to_string(i));
+//        cout << h["ishay_" + std::to_string(i)] << endl;
+    }
+
     cout << h.size() << endl;
-    cout << h.containsKey("ishay") << endl;
-    cout << h.insert("ishay", 24) << endl;
-    cout << h.size() << endl;
-    cout << h.bucketSize("ishay") << endl;
-    cout << h.bucketSize("mor") << endl;
+    cout << h.capacity() << endl;
+//    cout << h.at("ishay_0") << endl;
+
+//    cout << h.size() << endl;
+//    cout << h.containsKey("ishay") << endl;
+//    cout << h.insert("ishay", 24) << endl;
+//    cout << h.size() << endl;
+//    cout << h.bucketSize("ishay") << endl;
+//    cout << h.bucketSize("mor") << endl;
+//    cout << h.at("ishay") << endl;
+//    cout << h["mor"] << endl;
+
+
+//    HashMap<string, string> h1;
+//    cout << h1.size() << endl;
+//    cout << h1.containsKey("ishay") << endl;
+//    cout << h1.insert("ishay", "24") << endl;
+//    cout << h1.size() << endl;
+//    cout << h1.bucketSize("ishay") << endl;
+//    cout << h1.bucketSize("mor") << endl;
+//    cout << h1.at("ishay") << endl;
+//    h1.at("ishay") = "new";
+//    cout << h1.at("ishay") << endl;
+//    cout << h1["mor"] << endl;
 //    std::cout << "hi";
     return 1;
 }
--- ex3/HashMap.hpp
+++ ex3/HashMap.hpp
@@ -2,11 +2,13 @@
 // Created by Ishay Hil on 11/01/2020.
 //
 
-#ifndef HASHMA_HPP
-#define HASHMA_HPP
+#ifndef HASHMAP_HPP
+#define HASHMAP_HPP
 
 #include <list>
 #include <string>
+#include <csignal>
+#include <iostream>
 
 using std::list;
 using std::pair;
@@ -18,28 +20,100 @@ private:
     typedef pair<KeyT, ValueT> DictPair;
     int _size;
     int _capacity;
+    ValueT _undefined_val = ValueT();
     const double _LOWER_LOAD_FACTOR = 1.0 / 4;
     const double _UPPER_LOAD_FACTOR = 3.0 / 4;
-//    struct Node
-//    {
-//        ValueT val;
-//        Node *next;
-//        Node(ValueT val = ValueT(), Node *next = nullptr)
-//            : val(val), next(next)
-//        {}
-//    };
-    list<DictPair> map[16];
-    list<DictPair> &_bucket(KeyT key)
+    const double _INCREASE_FACTOR = 2;
+    const double _DECREASE_FACTOR = 1.0 / 2;
+    static const int _BASE_CAPACITY = 16;
+
+    list<DictPair> *_map;
+    list<DictPair> *_bucket(const KeyT key)
     {
-        return map[bucketIndex(key)];
+        return &(_map[bucketIndex(key)]);
     }
+
+    list<DictPair> *_bucket(const KeyT key) const
+    {
+        return &(_map[bucketIndex(key)]);
+    }
+
+    DictPair *_tuplePointer(const KeyT key)
+    {
+        list<DictPair> *bucket = _bucket(key);
+
+        for (auto &tuple: *bucket)
+        {
+            if (tuple.first == key)
+            {
+                return &tuple;
+            }
+        }
+        return nullptr;
+    }
+
+    DictPair *_tuplePointer(const KeyT key) const
+    {
+        list<DictPair> *bucket = _bucket(key);
+
+        for (auto &tuple: *bucket)
+        {
+            if (tuple.first == key)
+            {
+                return &tuple;
+            }
+        }
+        return nullptr;
+    }
+
+    void _rehash(const double factor)
+    {
+        list<DictPair> *old = new list<DictPair>[_capacity];
+        int old_capacity = _capacity;
+        for (int i = 0; i < _capacity; ++i)
+        {
+            old[i] = _map[i];
+        }
+        delete[] _map;
+        _capacity *= factor;
+        _size = 0;
+        _map = new list<DictPair>[_capacity];
+        for (int i = 0; i < old_capacity; i++)
+        {
+            for (auto &tuple: old[i])
+            {
+                insert(tuple.first, tuple.second);
+            }
+        }
+        delete[] old;
+    }
+
+    struct KeyEquals
+    {
+        KeyT key;
+        explicit KeyEquals(KeyT key)
+            : key(key)
+        {}
+        bool operator()(const DictPair &tuple)
+        {
+            return tuple.first == key;
+        }
+    };
 
 public:
     HashMap()
-        : _size(0), _capacity(16)
-    {}
+        : _size(0), _capacity(_BASE_CAPACITY)
+    {
+        _map = new list<DictPair>[_capacity];
+    }
 
-    int bucketIndex(KeyT key) const
+    ~HashMap()
+    {
+        delete[] _map;
+        _map = nullptr;
+    }
+
+    int bucketIndex(const KeyT key) const
     {
         int hash = std::hash<KeyT>{}(key);
         return hash & (_capacity - 1);
@@ -60,42 +134,104 @@ public:
         return _capacity == 0;
     }
 
-    bool insert(KeyT key, ValueT val) // todo why would not it work?
+    bool insert(const KeyT key, const ValueT val) // todo why would not it work?
     {
-
-        list<DictPair> &bucket = _bucket(key);
-        for (auto tuple: bucket)
+        if (containsKey(key))
         {
-            if (tuple.second == val)
-            {
-                return false;
-            }
+            return false;
         }
-        bucket.push_back(pair<KeyT, ValueT>(key, val));
+        list<DictPair> *bucket = _bucket(key);
+        bucket->push_back(pair<KeyT, ValueT>(key, val));
         _size++;
+        if (getLoadFactor() > _UPPER_LOAD_FACTOR)
+        {
+            _rehash(_INCREASE_FACTOR);
+        }
         return true;
-//        while (head->next != nullptr)
-//        {
-//            head = head->next;
-//        }
-//        head->next = val;
-//        return true;
     }
 
-    bool containsKey(KeyT key)
+    bool containsKey(const KeyT key) const
     {
-        return _bucket(key).size() > 0;
+        return _tuplePointer(key) != nullptr;
     }
 
-    int bucketSize(KeyT key)
+    int bucketSize(const KeyT key) const
     {
-        return _bucket(key).size();
+        return _bucket(key)->size();
     }
 
-    double getLoadFactor()
+    double getLoadFactor() const
     {
         return _size * 1.0 / _capacity;
     }
 
+    ValueT &at(const KeyT key)
+    {
+        DictPair *tuple = _tuplePointer(key);
+        if (tuple == nullptr)
+        {
+            throw std::out_of_range("key not found!");
+        }
+        else
+        {
+            return tuple->second;
+        }
+    }
+
+    ValueT at(const KeyT key) const
+    {
+        DictPair *tuple = _tuplePointer(key);
+        if (tuple == nullptr)
+        {
+            throw std::out_of_range("key not found!");
+        }
+        else
+        {
+            return tuple->second;
+        }
+    }
+
+    ValueT &operator[](const KeyT key) noexcept
+    {
+        DictPair *tuple = _tuplePointer(key);
+        if (tuple == nullptr)
+        {
+            return _undefined_val;
+        }
+        else
+        {
+            return tuple->second;
+        }
+    }
+
+    ValueT operator[](const KeyT key) const noexcept
+    {
+        DictPair *tuple = _tuplePointer(key);
+        if (tuple == nullptr)
+        {
+            return _undefined_val;
+        }
+        else
+        {
+            return tuple->second;
+        }
+    }
+
+    bool erase(const KeyT key)
+    {
+        if (!containsKey(key))
+        {
+            return false;
+        }
+        KeyEquals keyEquals(key);
+        list<DictPair> *bucket = _bucket(key);
+        bucket->remove_if(keyEquals);
+        _size--;
+        if (getLoadFactor() < _LOWER_LOAD_FACTOR && _capacity >= 2)
+        {
+            _rehash(_DECREASE_FACTOR);
+        }
+        return true;
+    }
 };
-#endif //HASHMA_HPP
+#endif //HASHMAP_HPP
